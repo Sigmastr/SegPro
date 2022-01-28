9020da5e52bbbcd11a6b42e81f8d82133d3395d6
Sigmastr <cristianmonttfierro@gmail.com>
2022-01-28 19:48:14 -0300

    Implementación PHP mailer, funciona en la web pero no en localhost

--- PhP/INSERTAR_EMPLEADO.php
+++ PhP/INSERTAR_EMPLEADO.php
@@ -1,6 +1,14 @@
 <?php
 include('../Conexion/conexion.php');
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Exception.php';
+require 'PHPMailer.php';
+require 'SMTP.php';
+
+
 $NombreEmpleado = $_POST["NombreEmpleado"];
 $RutEmpleado = $_POST["RutEmpleado"];
 $telefono = $_POST["telefono"];
@@ -13,6 +21,7 @@ for ($pass = '', $n = strlen($caracteres) - 1; strlen($pass) < $longpalabra;) {
     $x = rand(0, $n);
     $pass .= $caracteres[$x];
 }
+$pass2 = $pass;
 $pass = password_hash($pass, PASSWORD_DEFAULT);
 
 
@@ -21,11 +30,25 @@ $insertar = "INSERT INTO empleado(Rut_Empleado,Nombre,contrasena,Email,id_cargo,
 VALUES ( '$RutEmpleado','$NombreEmpleado','$pass','$Email','$Cargo','$Cuadrilla','$telefono')";
 
 $resultado = mysqli_query($conn, $insertar);
-
+//crear el envío por mail de la cuenta y la pass
 if ($resultado) {
-    echo "<script>alert('Registrado con éxito')</script>";
+    $header = 'From:' . $Email . "\r\n";
+    $header = "X-Mailer: PHP/" . phpversion() . "\r\n";
+    $header = "Mime-Version: 1.0 \r\n";
+    $header = "Content-Type: text/plain";
+
+    $message = "¡Bienvenido a la plataforma de gestión de SegProject!" . "\r\n";
+    $message .= "Para acceder a la plataforma debe utilizar su correo: " . $Email .  "\r\n";
+    $message .= "Su contraseña es:" . $pass2 . "\r\n";
+    $message .= "No olvide cambiar su contraseña al ingresar a la plataforma";
+
+    $para = $Email;
+    $matter = 'Registro en Software de gestión SegProject';
+    mail($para, $matter, utf8_decode($message), $header);
+
+
+    // echo "<script>alert('Registrado con éxito')</script>";
     header('Location:../Apartados/Empleado/ConfiguracionEmpleado.php ');
     die;
 } else {
-    echo $NombreEmpleado;
 }
